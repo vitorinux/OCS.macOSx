4607dea5de6cf88b266c32c4af7f7e03e2a6b00b
vmgit <victoriususm@gmail.com>
2018-04-04 00:46:03 +0200

    Fix quotes

--- 15.ModelName.sh
+++ 15.ModelName.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-system_profiler SPHardwareDataType | grep Model\ Name | cut -d : -f2 | sed 's/^ *//'
+system_profiler SPHardwareDataType | grep "Model Name" | cut -d : -f2 | sed 's/^ *//'
