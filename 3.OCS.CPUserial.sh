35577af1c22139c2853d713f6d1cb42e2479cef7
vmgit <victoriususm@gmail.com>
2018-04-03 23:52:54 +0200

    Get Hardware UUID properly

--- 3.OCS.CPUserial.sh
+++ 3.OCS.CPUserial.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-dmidecode | grep ID | cut -d : -f2 | head -1 | sed 's/^ *//'
+#dmidecode | grep ID | cut -d : -f2 | head -1 | sed 's/^ *//'
+system_profiler SPHardwareDataType | grep UUID | cut -d : -f2 | sed 's/^ *//'
