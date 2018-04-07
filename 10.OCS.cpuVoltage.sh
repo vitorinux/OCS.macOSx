0ecbcfce7bcf2b4088f77b3b1a058bf72547de50
vmgit <victoriususm@gmail.com>
2018-04-08 00:03:07 +0200

    Clean code and add some new features

--- 10.OCS.cpuVoltage.sh
+++ 10.OCS.cpuVoltage.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+## Uncomment for MacBook series
+## SYSTEM PROFILER Power Data Type Wattage
 #system_profiler -detailLevel full SPPowerDataType | grep Wattage
 #ioreg -l | grep LegacyBatteryInfo | cut -d '{' -f 2 | tr -d \} | tr ',' '=' | awk -F'=' '{print ($2*$10/10^22)}'
+## Depends of BREW PACKAGE and DMIDECODE package
 dmidecode 2>&1 | grep Voltage | cut -d: -f2 | head -1 | sed 's/^ *//'
--- CPUinfo.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-system_profiler SPHardwareDataType | grep UUID | cut -d : -f2 | sed 's/^ *//'
